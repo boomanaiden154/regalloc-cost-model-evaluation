afb002c72c8be8d97e7e6038125cfc3d3bdb8621
Aiden Grossman <agrossman154@yahoo.com>
2022-12-17 02:29:38 +0000

    Updated test_file.sh to fix a couple issues

--- scripts/test_file.sh
+++ scripts/test_file.sh
@@ -1,8 +1,9 @@
-for i in {1..30}
+set -e
+for i in {1..31}
 do
-    /scripts/pgo_compile.sh test.cpp test-$i /warmstart-1/saved_policy
-    /scripts/benchmark.sh ./test-$i test-$i.benchmark.txt
-    python3 /scripts/regalloc_score_parsing.py test-$i.regallocscoring.txt >> results.txt
+    /regalloc-testing/scripts/pgo_compile.sh $1 test-$i /warmstart/saved_policy
+    /regalloc-testing/scripts/benchmark.sh ./test-$i test-$i.benchmark.txt
+    python3 /regalloc-testing/scripts/regalloc_score_parsing.py test-$i.regallocscoring.txt >> results.txt
     echo -n "," >> results.txt
-    python3 /scripts/benchmark_average.py test-$i.benchmark.txt >> results.txt
+    python3 /regalloc-testing/scripts/benchmark_average.py test-$i.benchmark.txt >> results.txt
 done
