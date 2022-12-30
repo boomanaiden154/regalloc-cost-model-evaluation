e50e654223164c83559a69531db7820bdebde48c
Aiden Grossman <agrossman154@yahoo.com>
2022-12-30 01:22:15 +0000

    Added preliminary script to fit new weights

---
+++ scripts/fit_weights.py
@@ -0,0 +1,23 @@
+# This script takes in raw regalloc scoring data (ie not concatenated with
+# weights) and timing data and then fits new weights to try and best predict
+# the timing data.
+# Generate the raw data from a set of results from test_file.sh with
+# combined_regalloc_raw.sh
+# Usage: python3 fit_weights.py <combined_regalloc_raw.sh output>
+
+import sys
+import pandas
+
+from sklearn import linear_model
+
+if __name__ == '__main__':
+    if(len(sys.argv) != 2):
+        print("Usage is python3 fit_weights.py <combined_regalloc_raw.sh>")
+    df = pandas.read_csv(sys.argv[1], names=["copies","loads","stores","loadstores","expensiveremats","cheapremats","time"])
+    X = df[["copies","loads","stores","loadstores","expensiveremats","cheapremats"]]
+    # Scale the time so we don't get extremely small coefficients
+    y = df["time"] * 10 ** 9
+    regression = linear_model.LinearRegression()
+    regression.fit(X, y)
+    print(regression.coef_)
+    print(regression.score(X,y))
