1f262c4b33d60faacff83e048f709294f8a668f4
Aiden Grossman <agrossman154@yahoo.com>
2023-01-05 05:38:07 +0000

    Gave fit_weights.py support for multiple output types

--- scripts/fit_weights.py
+++ scripts/fit_weights.py
@@ -8,14 +8,24 @@
 import sys
 import pandas
 
+from absl import app, flags
 from sklearn import linear_model
 
 from evaluate_no_weights import evaluateModel
 
-if __name__ == '__main__':
-    if(len(sys.argv) != 2):
-        print("Usage is python3 fit_weights.py <combined_regalloc_raw.sh>")
-    df = pandas.read_csv(sys.argv[1], names=["copies","loads","stores","loadstores","expensiveremats","cheapremats","time"])
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string("input_file", None,
+                    "The path to the input file (output from combined_regalloc_raw.sh")
+flags.DEFINE_enum("output", "default", ["default", "coefficients", "intercept",
+                                        "cofint", "normcof", "r2", "posregpol",
+                                        "posregdif"],
+                  "The output type to use")
+
+flags.mark_flag_as_required("input_file")
+
+def main(_):
+    df = pandas.read_csv(FLAGS.input_file, names=["copies","loads","stores","loadstores","expensiveremats","cheapremats","time"])
     X = df[["copies","loads","stores","expensiveremats","cheapremats"]]
     # add loads and stores from loadStores
     X = X.assign(loads=(X["loads"] + df["loadstores"]))
@@ -25,15 +35,11 @@ if __name__ == '__main__':
     regression = linear_model.LinearRegression()
     regression.fit(X, y)
     coefficients = regression.coef_
-    print(f"Multivariable regression coefficients:{coefficients}")
-    print(f"Multivariable regression intercept:{regression.intercept_}")
     # normalize coefficients
     normalizationFactor = 1 / coefficients[0]
     normalizedCoefficients = []
     for coefficient in coefficients:
         normalizedCoefficients.append(coefficient * normalizationFactor)
-    print(f"Normalized coefficients:{normalizedCoefficients}")
-    print(f"Unadjusted R^2 value:{regression.score(X,y)}")
     predictedValues = regression.predict(X)
     scoreTimePairs = []
     for i in range(0,len(predictedValues)):
@@ -41,5 +47,31 @@ if __name__ == '__main__':
         time = df["time"][i]
         scoreTimePairs.append((score,time))
     newModelEvaluated = evaluateModel(scoreTimePairs)
-    print(f"Post regression polarity correct:{newModelEvaluated[0]}")
-    print(f"Post regression average difference:{newModelEvaluated[1]}")
+
+    if FLAGS.output == "default":
+        print(f"Multivariable regression coefficients:{coefficients}")
+        print(f"Multivariable regression intercept:{regression.intercept_}")
+        print(f"Normalized coefficients:{normalizedCoefficients}")
+        print(f"Unadjusted R^2 value:{regression.score(X,y)}")
+        print(f"Post regression polarity correct:{newModelEvaluated[0]}")
+        print(f"Post regression average difference:{newModelEvaluated[1]}")
+    elif FLAGS.output == "coefficients":
+        print(f"{coefficients[0]} {coefficients[1]} {coefficients[2]} {coefficients[3]} {coefficients[4]}")
+    elif FLAGS.output == "intercept":
+        print(f"{regression.intercept_}")
+    elif FLAGS.output == "cofint":
+        print(f"{coefficients[0]} {coefficients[1]} {coefficients[2]} {coefficients[3]} {coefficients[4]}")
+        print(f"{regression.intercept_}")
+    elif FLAGS.output == "normcof":
+        print((f"{normalizedCoefficients[0]} {normalizedCoefficients[1]}"
+               f"{normalizedCoefficients[2]} {normalizedCoefficients[3]}"
+               f"{normalizedCoefficients[4]}"))
+    elif FLAGS.output == "r2":
+        print(f"{regression.score(X,y)}")
+    elif FLAGS.output == "posregpol":
+        print(f"{newModelEvaluated[0]}")
+    elif FLAGS.output == "posregdif":
+        print(f"{newModelEvaluated[1]}")
+
+if __name__ == "__main__":
+    app.run(main)
--- scripts/multiple/fit_weights_extraction.sh
+++ scripts/multiple/fit_weights_extraction.sh
@@ -1,14 +1,15 @@
 # This script is designed to extract certain results from each sample of a
 # collection
 # Usage:
-# fit_weights_extraction.sh <query> <output file>
-# The query is used to grep the specific line that you want
+# fit_weights_extraction.sh <output type> <output file>
+# The output type is the type of data to grab. Run scripts/fit_weighs.py to
+# see possible values.
 set -e
 for i in {1..30}
 do
     echo "working on iteration $i"
     cd iteration-$i
-    value=$(python3 /regalloc-testing/scripts/fit_weights.py test-combined.txt | grep "$1")
+    value=$(python3 /regalloc-testing/scripts/fit_weights.py --input_file=test-combined.txt --output=$1)
     echo ${value##$1} >> ../$2
     cd ..
 done
