3c929aaca5a378f6af9834c3d4eac9dd4f2b8af2
Aiden Grossman <agrossman154@yahoo.com>
2023-04-03 23:36:20 +0000

    Got l1d benchmark to spill sufficiently

--- benchmarks/no-l1d-misses.c
+++ benchmarks/no-l1d-misses.c
@@ -1,5 +1,5 @@
 // This function should only hit L1D cache (maybe after some initial cache
-// warming) as it only allocates a (couple) pages of memory.
+// warming) as it only allocates a (couple) page(s) of memory.
 
 #include <unistd.h>
 #include <stdio.h>
@@ -12,24 +12,55 @@ __attribute__((always_inline))
 #else
 __attribute__((noinline))
 #endif
-void eightWideOperation(int* inputArray, size_t arraySize) {
-  for(int i = 0; i < arraySize; i += 8) {
-    int a = inputArray[i + 0];
-    int b = inputArray[i + 1];
-    int c = inputArray[i + 2];
-    int d = inputArray[i + 3];
-    int e = inputArray[i + 4];
-    int f = inputArray[i + 5];
-    int g = inputArray[i + 6];
-    int h = inputArray[i + 7];
-    int result = (a + b) + (c - d) + (e * f) + (g * h);
+void sixteenWideOperation(long* inputArray, size_t arraySize) {
+  long a = 0;
+  long b = 0;
+  long c = 0;
+  long d = 0;
+  long e = 0;
+  long f = 0;
+  long g = 0;
+  long h = 0;
+  long a1 = 0;
+  long b1 = 0;
+  long c1 = 0;
+  long d1 = 0;
+  long e1 = 0;
+  long f1 = 0;
+  long g1 = 0;
+  long h1 = 0;
+  for(int i = 0; i < arraySize; i += 16) {
+    a += inputArray[i + 0];
+    b += inputArray[i + 1];
+    c += inputArray[i + 2];
+    d += inputArray[i + 3];
+    e += inputArray[i + 4];
+    f += inputArray[i + 5];
+    g += inputArray[i + 6];
+    h += inputArray[i + 7];
+    a1 += inputArray[i + 8];
+    b1 += inputArray[i + 9];
+    c1 += inputArray[i + 10];
+    d1 += inputArray[i + 11];
+    e1 += inputArray[i + 12];
+    f1 += inputArray[i + 13];
+    g1 += inputArray[i + 14];
+    h1 += inputArray[i + 15];
+    long result = (a + b) + (c - d) + (e * f) + (g * h);
+    long result2 = (a1 + b1) + (c1 - d1) + (e1 * f1) + (g1 * h1);
     // cap values to try and prevent overflows
     if (result > 100000) {
       result = 500;
     } else if (result < -100000) {
       result = -500;
     }
-    inputArray[i + 0] = result;
+
+    if (result2 > 100000) {
+      result2 = 500;
+    } else if (result < -100000) {
+      result2 = -500;
+    }
+    inputArray[i + 0] = result + result2;
   }
 }
 
@@ -38,10 +69,10 @@ __attribute__((always_inline))
 #else
 __attribute__((noinline))
 #endif
-void leftShift(int* inputArray, size_t arraySize) {
-  int tempValue = inputArray[0];
+void leftShift(long* inputArray, size_t arraySize) {
+  long tempValue = inputArray[0];
   for(int i = 1; i < arraySize; ++i) {
-    int tempValue2 = inputArray[i];
+    long tempValue2 = inputArray[i];
     inputArray[i] = tempValue;
     tempValue = tempValue2;
   }
@@ -51,15 +82,15 @@ void leftShift(int* inputArray, size_t arraySize) {
 int main() {
   int pageSize = getpagesize();
   printf("current page size:%d\n", pageSize);
-  size_t arraySize = (1 * pageSize) / sizeof(int);
-  int* intArray = (int*)mmap(NULL, arraySize * sizeof(int), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  printf("allocated memory is at:%ld\n", (long)intArray);
+  size_t arraySize = (1 * pageSize) / sizeof(long);
+  long* longArray = (long*)mmap(NULL, arraySize * sizeof(long), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  printf("allocated memory is at:%ld\n", (long)longArray);
   printf("array size:%ld\n", arraySize);
-  memset(intArray, 5, arraySize);
+  memset(longArray, 5, arraySize);
   for(int i = 0; i < 2500000; ++i) {
-      eightWideOperation(intArray, arraySize);
-      leftShift(intArray, arraySize);
+      sixteenWideOperation(longArray, arraySize);
+      leftShift(longArray, arraySize);
   }
-  munmap(intArray, arraySize * sizeof(int));
+  munmap(longArray, arraySize * sizeof(long));
   return 0;
 }
